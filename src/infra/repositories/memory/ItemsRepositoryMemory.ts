687487fefdfdafcbe3a7c931dbb9662294471e98
Gabriel Belther <belthergabriel@gmail.com>
2023-01-28 11:25:46 -0300

    feat: create items repository memory

---
+++ src/infra/repositories/memory/ItemsRepositoryMemory.ts
@@ -0,0 +1,11 @@
+import { Item } from '../../../entities';
+import { ItemsRepository } from '../../../repository';
+
+export class ItemsRepositoryMemory implements ItemsRepository {
+  public items: Item[] = [];
+
+  async findById(itemId: string): Promise<Item | undefined> {
+    const item = this.items.find((item) => item.id === itemId);
+    return item;
+  }
+}
