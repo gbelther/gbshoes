704bcfd3b478b0a767a69cb0180972ad71d29499
Gabriel Belther <belthergabriel@gmail.com>
2023-01-29 14:09:13 -0300

    refactor: validation on constructor

--- src/domain/entities/Customer.ts
+++ src/domain/entities/Customer.ts
@@ -1,11 +1,26 @@
+import { randomUUID } from 'node:crypto';
 import { Cpf } from './Cpf';
 
+type Params = {
+  id?: string;
+  name: string;
+  cpfData: string;
+  email: string;
+  password: string;
+};
+
 export class Customer {
-  constructor(
-    readonly id: string,
-    readonly name: string,
-    readonly cpf: Cpf,
-    readonly email: string,
-    readonly password: string
-  ) {}
+  id: string;
+  name: string;
+  cpf: Cpf;
+  email: string;
+  password: string;
+
+  constructor({ id, name, cpfData, email, password }: Params) {
+    this.id = id ?? randomUUID();
+    this.name = name;
+    this.cpf = new Cpf(cpfData);
+    this.email = email;
+    this.password = password;
+  }
 }
