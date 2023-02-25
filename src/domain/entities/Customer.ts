78e56de96657f1d25ecc93243f05b8c8aedc0a0b
Gabriel Belther <belthergabriel@gmail.com>
2023-02-25 12:40:10 -0300

    feat: validação na instanciação de Customer

--- src/domain/entities/Customer.ts
+++ src/domain/entities/Customer.ts
@@ -18,9 +18,29 @@ export class Customer {
 
   constructor({ id, name, cpfData, email, password }: Params) {
     this.id = id ?? randomUUID();
+    this.setName(name);
+    this.setCpf(cpfData);
+    this.setEmail(email);
+    this.setPassword(password);
+  }
+
+  setName(name: string) {
+    if (!name) throw new Error('O campo nome é obrigatório.');
     this.name = name;
-    this.cpf = new Cpf(cpfData);
+  }
+
+  setCpf(cpfData: string) {
+    const cpf = new Cpf(cpfData);
+    this.cpf = cpf;
+  }
+
+  setEmail(email: string) {
+    if (!email) throw new Error('O campo E-mail é obrigatório.');
     this.email = email;
+  }
+
+  setPassword(password: string) {
+    if (!password) throw new Error('O campo senha é obrigatório.');
     this.password = password;
   }
 }
