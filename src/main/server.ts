4db5e45394935111493db50c4146b7518b7a50f0
Gabriel Belther <belthergabriel@gmail.com>
2023-02-25 12:57:20 -0300

    feat: create API express

---
+++ src/main/server.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import express, { NextFunction, Request, Response } from 'express';
+import { router } from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(router);
+
+app.use(
+  (
+    error: Error,
+    _request: Request,
+    response: Response,
+    _next: NextFunction
+  ) => {
+    return response.status(400).json({ error: error.message });
+  }
+);
+
+app.listen(3333, () => {
+  console.log('Server is running on port 3333!');
+});
