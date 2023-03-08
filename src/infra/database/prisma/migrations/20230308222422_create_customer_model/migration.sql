cc932c8335b86856407593dcf9d73e14a4fbc285
Gabriel Belther <belthergabriel@gmail.com>
2023-03-08 19:31:11 -0300

    refactor: recriate migration CreateCustomerModel

--- src/infra/database/prisma/migrations/20230308222422_create_customer_model/migration.sql
+++ src/infra/database/prisma/migrations/20230308222422_create_customer_model/migration.sql
@@ -5,7 +5,7 @@ CREATE TABLE "customers" (
     "cpf" VARCHAR(11) NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "customers_pkey" PRIMARY KEY ("id")
 );
